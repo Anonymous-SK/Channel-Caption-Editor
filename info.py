7278f6f07675936fdb24befdc94fe31cb3f4cf8e
Anonymous <78583783+Anonymous-SK@users.noreply.github.com>
2021-07-17 15:18:07 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,7 +1,6 @@
 # Ns Bots
 
 import os
-from main import owner
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 API_ID = int(os.environ.get("API_ID", 12345))
@@ -11,20 +10,3 @@ BUTTON_NAME = os.environ.get("BUTTON_NAME", "")
 CAPTION = os.environ.get("FILES_CAPTION", "")
 OWNER_ID = int(os.environ.get("OWNER_ID", ""))
 OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "")
-
-about_text = f"""--**My Details:**--
-
-🤖 𝐌𝐲 𝐍𝐚𝐦𝐞: Caption Editor Bot
-    
-📝 𝐋𝐚𝐧𝐠𝐮𝐚𝐠𝐞: [Python 3](https://www.python.org/)
-
-🧰 𝐅𝐫𝐚𝐦𝐞𝐰𝐨𝐫𝐤: [Pyrogram](https://github.com/pyrogram/pyrogram)
-
-👨‍💻 Developer : {owner.mention(style='md')} 
-
-📢 𝐂𝐡𝐚𝐧𝐧𝐞𝐥: [NS BOT UPDATES](https://t.me/Ns_bot_updates)
-
-👥 𝐆𝐫𝐨𝐮𝐩: [Ns BOT SUPPORT](https://t.me/Ns_Bot_supporters)
-
-📎 SOURCE CODE : [Click Here 😍](https://github.com/Anonymous-SK/Channel-Caption-Editor)
-"""
