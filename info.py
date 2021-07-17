30155d85dc5142d194e296721fc7bdbc0933a524
Anonymous <78583783+Anonymous-SK@users.noreply.github.com>
2021-07-17 10:34:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -10,3 +10,18 @@ BUTTON_NAME = os.environ.get("BUTTON_NAME", "")
 CAPTION = os.environ.get("FILES_CAPTION", "")
 OWNER_ID = int(os.environ.get("OWNER_ID", ""))
 OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "")
+
+about_text = f"""--**My Details:**--
+
+🤖 𝐌𝐲 𝐍𝐚𝐦𝐞: Movies Search NS bot
+    
+📝 𝐋𝐚𝐧𝐠𝐮𝐚𝐠𝐞: [Python 3](https://www.python.org/)
+
+🧰 𝐅𝐫𝐚𝐦𝐞𝐰𝐨𝐫𝐤: [Pyrogram](https://github.com/pyrogram/pyrogram)
+
+👨‍💻 Creator : [This Person](tg://user?id={OWNER_ID})
+
+📢 𝐂𝐡𝐚𝐧𝐧𝐞𝐥: [NS BOT UPDATES](https://t.me/Ns_bot_updates)
+
+👥 𝐆𝐫𝐨𝐮𝐩: [Ns BOT SUPPORT](https://t.me/Ns_Bot_supporters)
+"""
