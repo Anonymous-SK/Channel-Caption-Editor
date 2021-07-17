1b0f994cc18d8aa373c65c167253aca3d7e118cc
Anonymous <78583783+Anonymous-SK@users.noreply.github.com>
2021-07-17 09:05:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -2,10 +2,9 @@
 
 import os
 
-class config(object):
-    TG_BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    API_ID = int(os.environ.get("API_ID", 12345))
-    API_HASH = os.environ.get("API_HASH", "")
-    BUTTON_URL = os.environ.get("BUTTON_URL", "")
-    CAPTION = os.environ.get("FILES_CAPTION", "")
-
+TG_BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+API_ID = int(os.environ.get("API_ID", 12345))
+API_HASH = os.environ.get("API_HASH", "")
+BUTTON_URL = os.environ.get("BUTTON_URL", "")
+CAPTION = os.environ.get("FILES_CAPTION", "")
+OWNER_ID = int(is.environ.get("OWNER_ID", "")
