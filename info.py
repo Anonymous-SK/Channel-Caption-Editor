6b17bdd33787d578440cddd176646ba432c44863
Anonymous <78583783+Anonymous-SK@users.noreply.github.com>
2021-07-17 09:17:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -2,7 +2,7 @@
 
 import os
 
-TG_BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 API_ID = int(os.environ.get("API_ID", 12345))
 API_HASH = os.environ.get("API_HASH", "")
 BUTTON_URL = os.environ.get("BUTTON_URL", "")
