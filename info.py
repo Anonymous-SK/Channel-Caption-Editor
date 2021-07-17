75daa4c8ea91c06f612b710c576320215e55b7e4
Anonymous <78583783+Anonymous-SK@users.noreply.github.com>
2021-07-17 09:20:43 +0530

    Update info.py

--- info.py
+++ info.py
@@ -2,7 +2,7 @@
 
 import os
 
-class pruthvi(object):
+class ns(object):
   BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
   API_ID = int(os.environ.get("API_ID", 12345))
   API_HASH = os.environ.get("API_HASH", "")
