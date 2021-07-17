b98005c85309c26266a6973a391148eebd3e1ac0
Anonymous <78583783+Anonymous-SK@users.noreply.github.com>
2021-07-17 09:19:34 +0530

    Update info.py

--- info.py
+++ info.py
@@ -2,9 +2,10 @@
 
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", 12345))
-API_HASH = os.environ.get("API_HASH", "")
-BUTTON_URL = os.environ.get("BUTTON_URL", "")
-CAPTION = os.environ.get("FILES_CAPTION", "")
-OWNER_ID = int(is.environ.get("OWNER_ID", "")
+class pruthvi(object):
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+  API_ID = int(os.environ.get("API_ID", 12345))
+  API_HASH = os.environ.get("API_HASH", "")
+  BUTTON_URL = os.environ.get("BUTTON_URL", "")
+  CAPTION = os.environ.get("FILES_CAPTION", "")
+  OWNER_ID = int(is.environ.get("OWNER_ID", "")
