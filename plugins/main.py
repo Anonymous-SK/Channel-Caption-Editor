bb8219f194ec5f49a0683b83ec16b088f1245a6c
Anonymous <78583783+Anonymous-SK@users.noreply.github.com>
2021-07-17 10:05:57 +0530

    Update main.py

--- plugins/main.py
+++ plugins/main.py
@@ -2,6 +2,8 @@ from pyrogram import Client , filters
 from pyrogram.types import InlineKeyboardButton , InlineKeyboardMarkup
 from info import ns
 
+OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "")
+
 ns_keyboard = [
         [
             InlineKeyboardButton('My Owner', url=f"https://t.me/{OWNER_USERNAME}"),
