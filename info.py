719a20f4615c7441ed918c55c8403877b8f4f4df
Anonymous <78583783+Anonymous-SK@users.noreply.github.com>
2021-07-17 10:21:52 +0530

    Update info.py

--- info.py
+++ info.py
@@ -2,11 +2,11 @@
 
 import os
 
-class ns(object):
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-  API_ID = int(os.environ.get("API_ID", 12345))
-  API_HASH = os.environ.get("API_HASH", "")
-  BUTTON_URL = os.environ.get("BUTTON_URL", "")
-  BUTTON_NAME = os.environ.get("BUTTON_NAME", "")
-  CAPTION = os.environ.get("FILES_CAPTION", "")
-  OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+API_ID = int(os.environ.get("API_ID", 12345))
+API_HASH = os.environ.get("API_HASH", "")
+BUTTON_URL = os.environ.get("BUTTON_URL", "")
+BUTTON_NAME = os.environ.get("BUTTON_NAME", "")
+CAPTION = os.environ.get("FILES_CAPTION", "")
+OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "")
