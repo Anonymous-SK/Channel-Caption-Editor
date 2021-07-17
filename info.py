4ebf4cd4d54dcd830adbc5c236ed90a37e532af8
Anonymous <78583783+Anonymous-SK@users.noreply.github.com>
2021-07-17 09:41:29 +0530

    Update info.py

--- info.py
+++ info.py
@@ -7,7 +7,7 @@ class ns(object):
   API_ID = int(os.environ.get("API_ID", 12345))
   API_HASH = os.environ.get("API_HASH", "")
   BUTTON_URL = os.environ.get("BUTTON_URL", "")
+  BUTTON_NAME = os.environ.get("BUTTON_NAME", "")
   CAPTION = os.environ.get("FILES_CAPTION", "")
   OWNER_ID = int(os.environ.get("OWNER_ID", "")
-  BUTTON_NAME = os.environ.get("BUTTON_NAME", "")
   OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "")
