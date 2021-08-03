d5dbdaa6d278271b640ec4295cf444defe568a24
Anonymous <78583783+Anonymous-SK@users.noreply.github.com>
2021-08-03 09:00:05 +0530

    Update main.py

--- plugins/main.py
+++ plugins/main.py
@@ -72,7 +72,5 @@ async def cation_text(Client , message):
 
 @Client.on_message(filters.photo & filters.channel)
 async def photo_caption(Client , message):
-  if P_CAPTION == "True":
-        await message.edit(CAPTION , reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton(f"{BUTTON_NAME}", url=f"{BUTTON_URL}")]]))
-  else:
-	pass
+  if P_CAPTION =="True":
+    await message.edit(CAPTION , reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton(f"{BUTTON_NAME}", url=f"{BUTTON_URL}")]]))
