d0f158ed7d477d7dbfacfe51a56086299a928253
Raj ×_× <78583783+Raj-Anonymous@users.noreply.github.com>
2021-08-12 15:16:55 +0530

    Update main.py

--- plugins/main.py
+++ plugins/main.py
@@ -1,5 +1,6 @@
 from pyrogram import Client , filters
 from pyrogram.types import InlineKeyboardButton , InlineKeyboardMarkup
+from info import BUTTON_URL , BUTTON_NAME , CAPTION , OWNER_ID
 
 @Client.on_message(filters.document  & filters.channel)
 async def cation_text(Client , message):
